d79a01f58ab42f107378aabb88dc33e510d9f249
Ravi Tokas <ravitokas90@gmail.com>
2023-11-19 08:31:06 +0530

    add styles to nav

--- app/page.js
+++ app/page.js
@@ -3,11 +3,12 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <main>
-      <div className="text-center ">
+      <div className="flex flex-col items-center p-10 space-y-4">
         <h1>Welcome to FitJam</h1>
         <p>Stay motivated and break through plateaus</p>
         <Button>Get Started</Button>
       </div>
+      <hr />
     </main>
   );
 }
