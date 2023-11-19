6512b1f7b8795be04857731dbf012016bdf00379
Ravi Tokas <ravitokas90@gmail.com>
2023-11-19 10:18:27 +0530

    images component struggle

--- app/page.js
+++ app/page.js
@@ -7,8 +7,16 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import Image from "next/image";
 
-export default function Home() {
+export async function getPlateau() {
+  const result = await fetch("http://localhost:4000/plateaus");
+  return result.json();
+}
+
+export default async function Home() {
+  const plateaus = await getPlateau();
+
   return (
     <main>
       <header className="flex flex-col items-center p-10 space-y-4">
@@ -26,22 +34,33 @@ export default function Home() {
           <Button>Learn More</Button>
         </div>
         <div className="grid grid-cols-3 gap-8">
-          <Card>
-            <CardHeader>
-              <CardTitle>Plateau Breaking Guide</CardTitle>
-              <CardDescription>Card Description</CardDescription>
-            </CardHeader>
-            <CardContent>
-              <p>Implement deloading phases to help overcome plateaus</p>
-            </CardContent>
-            <CardFooter>
-              <p>Soon</p>
-            </CardFooter>
-          </Card>
+          {plateaus.map((plateau) => (
+            <Card key={plateau.id}>
+              <CardHeader>
+                <CardTitle>{plateau.title}</CardTitle>
+                <CardDescription>{plateau.description}</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <div>
+                  <Image
+                    src={plateau.image}
+                    alt={plateau.title}
+                    width={500}
+                    height={500}
+                  />
+                </div>
+              </CardContent>
+              <CardFooter>
+                <Button>Show more</Button>
+                <p>{plateau.level}</p>
+              </CardFooter>
+            </Card>
+          ))}
         </div>
       </section>
 
       <hr />
+      <h1>hello world</h1>
     </main>
   );
 }
