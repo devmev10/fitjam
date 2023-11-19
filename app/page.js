92a479809c50d72751e9c43b9bfb4d00a50c3e10
Ravi Tokas <ravitokas90@gmail.com>
2023-11-19 08:39:55 +0530

    add header

--- app/page.js
+++ app/page.js
@@ -3,11 +3,23 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <main>
-      <div className="flex flex-col items-center p-10 space-y-4">
+      <header className="flex flex-col items-center p-10 space-y-4">
         <h1>Welcome to FitJam</h1>
         <p>Stay motivated and break through plateaus</p>
         <Button>Get Started</Button>
-      </div>
+      </header>
+
+      <hr />
+
+      <section>
+        <div className="flex flex-col items-center p-10 space-y-4">
+          <h1>Personalized Plateau-Breaking Plans</h1>
+          <p>Overcome plateaus with tailored strategies</p>
+          <Button>Learn More</Button>
+        </div>
+        <div></div>
+      </section>
+
       <hr />
     </main>
   );
