29e90ea768e886bfad372bfac83a0c5271983a83
Ravi Tokas <ravitokas90@gmail.com>
2023-11-29 15:37:34 +0530

    update page.js

--- app/getstarted/page.js
+++ app/getstarted/page.js
@@ -1,3 +1,16 @@
+import { useState } from "react";
+
 export default function GetStarted() {
-  return <div>Get started</div>;
+  const [age, setAge] = useState();
+
+  return (
+    <main>
+      <form>
+        <label>
+          Age:{" "}
+          <input type="text" value={age} onChange={(e) => e.target.value} />
+        </label>
+      </form>
+    </main>
+  );
 }
