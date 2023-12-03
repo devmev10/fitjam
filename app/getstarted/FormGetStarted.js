c192a915f741c21f4637a5dc8af54ce90f3a6a0f
Ravi Tokas <ravitokas90@gmail.com>
2023-12-03 14:31:14 +0530

    Add gender entry

--- app/getstarted/FormGetStarted.js
+++ app/getstarted/FormGetStarted.js
@@ -3,6 +3,7 @@
 import { useState } from "react";
 export default function FormGetStarted() {
   const [age, setAge] = useState();
+  const [gender, setGender] = useState();
   return (
     <form>
       <label>
@@ -12,6 +13,23 @@ export default function FormGetStarted() {
           value={age}
           onChange={(e) => setAge(e.target.value)}
         />
+      </label>
+      <label>
+        Gender:{" "}
+        <input
+          type="radio"
+          value="Male"
+          checked={gender === "Male"}
+          onChange={(e) => setGender(e.target.value)}
+        />
+        Male
+        <input
+          type="radio"
+          value="Female"
+          checked={gender === "Female"}
+          onChange={(e) => setGender(e.target.value)}
+        />
+        Female
       </label>
     </form>
   );
