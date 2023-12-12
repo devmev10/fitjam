b1aca8e0b067346741cb1cc7e34021a2f0e19d83
Ravi Tokas <ravitokas90@gmail.com>
2023-12-12 16:34:38 +0530

    Update form

--- app/getstarted/FormGetStarted.js
+++ app/getstarted/FormGetStarted.js
@@ -77,7 +77,124 @@ export default function FormGetStarted() {
                   className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
                 />
               </div>
-              {/* Repeat for other fields */}
+              {/* Weight Input */}
+              <div>
+                <label
+                  htmlFor="weight"
+                  className="text-sm font-medium text-gray-700"
+                >
+                  Weight
+                </label>
+                <input
+                  id="weight"
+                  name="weight"
+                  type="text"
+                  placeholder="Your weight"
+                  className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                />
+              </div>
+
+              {/* Sleep Duration Input */}
+              <div>
+                <label
+                  htmlFor="sleepDuration"
+                  className="text-sm font-medium text-gray-700"
+                >
+                  Sleep Duration
+                </label>
+                <input
+                  id="sleepDuration"
+                  name="sleepDuration"
+                  type="text"
+                  placeholder="Average hours per night"
+                  className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                />
+              </div>
+
+              {/* Sleep Time Input */}
+              <div>
+                <label
+                  htmlFor="sleepTime"
+                  className="text-sm font-medium text-gray-700"
+                >
+                  Sleep Time
+                </label>
+                <input
+                  id="sleepTime"
+                  name="sleepTime"
+                  type="text"
+                  placeholder="Time you usually sleep"
+                  className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                />
+              </div>
+
+              {/* Wake Up Time Input */}
+              <div>
+                <label
+                  htmlFor="wakeUpTime"
+                  className="text-sm font-medium text-gray-700"
+                >
+                  Wake Up Time
+                </label>
+                <input
+                  id="wakeUpTime"
+                  name="wakeUpTime"
+                  type="text"
+                  placeholder="Time you usually wake up"
+                  className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                />
+              </div>
+
+              {/* Height Input */}
+              <div>
+                <label
+                  htmlFor="height"
+                  className="text-sm font-medium text-gray-700"
+                >
+                  Height
+                </label>
+                <input
+                  id="height"
+                  name="height"
+                  type="text"
+                  placeholder="Your height"
+                  className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                />
+              </div>
+
+              {/* Nationality Input */}
+              <div>
+                <label
+                  htmlFor="nationality"
+                  className="text-sm font-medium text-gray-700"
+                >
+                  Nationality
+                </label>
+                <input
+                  id="nationality"
+                  name="nationality"
+                  type="text"
+                  placeholder="Your nationality"
+                  className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                />
+              </div>
+
+              {/* Health Condition Input */}
+              <div>
+                <label
+                  htmlFor="healthCondition"
+                  className="text-sm font-medium text-gray-700"
+                >
+                  Health Condition?
+                </label>
+                <input
+                  id="healthCondition"
+                  name="healthCondition"
+                  type="text"
+                  placeholder="Any condition like Lower back issue"
+                  className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                />
+              </div>
             </div>
 
             {/* Diet Preference */}
